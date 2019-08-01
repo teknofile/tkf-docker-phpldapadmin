6e36ffe362e4ab94d32e894af203592a875f626f
James Richardson <teknofile@teknofile.org>
2019-07-31 22:55:19 -0600

    remove this run.sh stuff

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-  --name=test-phpldapadmin \
-  -e PUID="$(id -u)" \
-  -e PGID="$(id -g)" \
-  -v /home/jrichardson/Documents/code/teknofile/tkf-docker-phpldapadmin/test:/config \
-  -p 30080:80 \
-  teknofile/tkf-docker-phpldapadmin:devel
